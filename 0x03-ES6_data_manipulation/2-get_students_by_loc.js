3393edd7c2344a2a029b8231cfedcc9d9a28fc86
Kmangaty <kmaangaty@gmail.com>
2024-05-03 03:52:29 +0300

    setup

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 const getStudentsByLocation = (students, city) => {
-  const arratyObjec = students.filter((funtion) => funtion.location === city);
-  return arratyObjec;
+  const arrs = students.filter((ft) => ft.location === city);
+  return arrs;
 };
 
 export default getStudentsByLocation;
