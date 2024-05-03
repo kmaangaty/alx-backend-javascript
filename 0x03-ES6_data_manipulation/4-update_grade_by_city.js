b065217f41d71805616be1e1f2a353c46888e170
Kmangaty <kmaangaty@gmail.com>
2024-05-03 04:00:49 +0300

    setup

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,9 @@
-export default function updateStudentGradeByCity(stds, city, newGrades) {
-  if (!Array.isArray(stds) || !Array.isArray(newGrades)) {
+export default function updateStudentGradeByCity(stds, md, ngs) {
+  if (!Array.isArray(stds) || !Array.isArray(ngs)) {
     return [];
   }
-  const stc = stds.filter((student) => student.location === city).map((student) => {
-    const itt = newGrades.find((stt) => student.id === stt.studentId);
+  const stc = stds.filter((student) => student.location === md).map((student) => {
+    const itt = ngs.find((stt) => student.id === stt.studentId);
     return { ...student, grade: itt ? itt.grade : 'N/A' };
   });
   return stc;
