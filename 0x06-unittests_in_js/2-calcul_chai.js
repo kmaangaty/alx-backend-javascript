e5f24d23cf480b7ae233002a70621b863217a996
Kmangaty <kmaangaty@gmail.com>
2024-08-21 15:48:22 +0300

    setup 20

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,19 +1,18 @@
 function calculateNumber(type, a, b) {
-  const ra = Math.round(a);
-  const rb = Math.round(b);
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
 
   if (type === 'SUM') {
-    return ra + rb;
-  }
-  if (type === 'DIVIDE') {
-    if (rb === 0) {
+    return roundedA + roundedB;
+  } else if (type === 'SUBTRACT') {
+    return roundedA - roundedB;
+  } else if (type === 'DIVIDE') {
+    if (roundedB === 0) {
       return 'Error';
     }
-    return ra / rb;
+    return roundedA / roundedB;
   }
-  if (type === 'SUBTRACT') {
-    return ra - rb;
-  }
+  return null;
 }
 
 module.exports = calculateNumber;
