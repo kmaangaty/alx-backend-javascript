46b2d4016faa18f1c1489946bb4320f7a2da9283
Kmangaty <kmaangaty@gmail.com>
2024-08-21 16:02:36 +0300

    setup 23

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -5,13 +5,12 @@ describe('Testing numbers', () => {
     expect(1 === 1).to.be.true;
   });
 
-
-  it('6 is equal to 6', () => {
-    expect(6 === 6).to.be.true;
-  });
-
   it('2 is equal to 2', () => {
     expect(2 === 2).to.be.true;
+  });
+
+  it.skip('1 is equal to 3', () => {  // Skip this test
+    expect(1 === 3).to.be.true;
   });
 
   it('3 is equal to 3', () => {
@@ -22,16 +21,15 @@ describe('Testing numbers', () => {
     expect(4 === 4).to.be.true;
   });
 
-  it.skip('1 is equal to 3', () => {
-    expect(1 === 3).to.be.true;
-  });
-
   it('5 is equal to 5', () => {
     expect(5 === 5).to.be.true;
+  });
+
+  it('6 is equal to 6', () => {
+    expect(6 === 6).to.be.true;
   });
 
   it('7 is equal to 7', () => {
     expect(7 === 7).to.be.true;
   });
-
 });
