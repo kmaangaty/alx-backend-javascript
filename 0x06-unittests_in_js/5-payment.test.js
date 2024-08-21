249f68e97e607dfdb0c941855ee6fb8b01683727
Kmangaty <kmaangaty@gmail.com>
2024-08-21 15:58:01 +0300

    setup 22

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -2,28 +2,38 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./5-payment');
 
-describe('sendPaymentRequestToApi', () => {
-  let bigBrother;
+describe('sendPaymentRequestToApi', function () {
+  let consoleSpy;
 
-  beforeEach(() => {
-    if (!bigBrother) {
-      bigBrother = sinon.spy(console);
-    }
+  // Hook: before each test, set up the spy
+  beforeEach(function () {
+    consoleSpy = sinon.spy(console, 'log');
   });
 
-  afterEach(() => {
-    bigBrother.log.resetHistory();
+  // Hook: after each test, restore the spy
+  afterEach(function () {
+    consoleSpy.restore();
   });
 
-  it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
+  it('should log "The total is: 120" when called with 100 and 20', function () {
+    // Call the function
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.log.calledWith('The total is: 120')).to.be.true;
-    expect(bigBrother.log.calledOnce).to.be.true;
+
+    // Check that console.log was called with the correct string
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+
+    // Check that console.log was called only once
+    expect(consoleSpy.calledOnce).to.be.true;
   });
 
-  it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
+  it('should log "The total is: 20" when called with 10 and 10', function () {
+    // Call the function
     sendPaymentRequestToApi(10, 10);
-    expect(bigBrother.log.calledWith('The total is: 20')).to.be.true;
-    expect(bigBrother.log.calledOnce).to.be.true;
+
+    // Check that console.log was called with the correct string
+    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+
+    // Check that console.log was called only once
+    expect(consoleSpy.calledOnce).to.be.true;
   });
 });
