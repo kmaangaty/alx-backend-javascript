1dcf9dde9db21e37e8e0870053b2ad15926937d8
Kmangaty <kmaangaty@gmail.com>
2024-08-22 17:30:17 +0300

    setup 29

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,35 +1,13 @@
 const request = require('request');
 const { expect } = require('chai');
-const server = require('./api');
 
-describe('Index page', () => {
-  let serverInstance;
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-  before((done) => {
-    serverInstance = server;
-    done();
-  });
-
-  after((done) => {
-    if (serverInstance) {
-      serverInstance.close(done); // Ensure the server is properly closed
-    } else {
-      done(); // If serverInstance is not defined, just call done
-    }
-  });
-
-  it('should return status code 200', (done) => {
-    request.get('http://localhost:7865', (error, response) => {
-      if (error) return done(error);
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return the correct result', (done) => {
-    request.get('http://localhost:7865', (error, response, body) => {
-      if (error) return done(error);
-      expect(body).to.equal('Welcome to the payment system');
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
